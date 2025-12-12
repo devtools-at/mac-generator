11235ea0a5868dc5ea7443459cc2c4f7ff9a2e5c
DevTools.at <devtools@devtools.at>
2025-12-12 12:55:23 +0100

    Initial commit - npm package with TypeScript support

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * MAC Address Generator
+ * Generate random MAC addresses
+ *
+ * Online tool: https://devtools.at/tools/mac-generator
+ *
+ * @packageDocumentation
+ */
+
+function createIdGenerator() {
+  let id = 0;
+  return () => id++;
+}
+
+function generateRandomMac(
+  format: FormatType,
+
+// Export for convenience
+export default { encode, decode };
